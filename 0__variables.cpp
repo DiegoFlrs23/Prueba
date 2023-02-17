b3ef0ec98d83683ad7c2e435c5d7af07c0172ea5
Diego Flores <diegoflrs2321@gmail.com>
2023-02-16 21:22:06 -0600

    Segundo Commit

---
+++ 0__variables.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std; //donde se encuentran todas las librerias estandar
+main(){
+	/*
+	//Variable: es un espacio de memoria reservado para almacenar un valor que corresponde a un tipo de dato
+	//cout<<"Hola mundo..."<<endl;
+	//tipo de nombres de la variable
+	//c++ case-senitive: distincion entre mayusculas y minusculas
+	//Estilo de nombres para variables: camel case
+	//Para las clases de metodos y funciones lowerCamelCase y para los variables Snake Case
+	//Restriccion el nombre de una variable no puede: Iniciar con numero, no puede tener caracteres especiales, no espacios
+	string nombreCompleto = "Alex"; //lowerCamelCase
+	string NombreCompleto = "Alex 2"; //UpperCamelCase
+	string nombre_completo =  "Alex 3";	//SnakeCase
+	cout<<nombreCompleto<<endl;
+	cout<<NombreCompleto<<endl;
+	cout<<nombre_completo<<endl;
+	system("pause");
+	
+	
+	//Variables numericas
+	//Numeros enteros
+	//Como declarar una variable: tipo-nombre de la variable-valor-;
+	short entero_corto = 32767; //2 Bytes (-32,768 a 32,767)
+	int entero = 2147483647; //4 Bytes (-2,147,483,648 a 2,147,483,647)
+	long long entero_largo = 12147483648; //8 Bytes
+	unsigned long long int i_entero_largo = 18446744073709551615; //10 Bytes
+	cout<<"short: "<<entero_corto<<endl;
+	cout<<"int: "<<entero<<endl;
+	cout<<"long: "<<entero_largo<<endl;
+	cout<<"long int: "<<i_entero_largo<<endl;
+	
+	
+	//Numeros reales de punto flotante
+	float numero_decimal = 100.235; //4 Bytes
+	double numero_decimal_2 = 100.236; //8 Bytes
+	long double numero_decimal_3 = 100.237; //10 Bytes
+	cout<<"float: "<<numero_decimal<<endl;
+	cout<<"double: "<<numero_decimal_2<<endl;
+	cout<<"long double: "<<numero_decimal_3<<endl;
+
+	
+	char cr = 'T'; //El carcater va entre comillas
+	char asc = 64; //Tabla de caracteres del codigo asc
+	cout<<"char: "<<cr<<endl;
+	cout<<"char: "<<asc<<endl;
+	
+	
+	//Logico 1 o 0, true or false
+	bool logico	= true;
+	bool logico_2 = false;
+	bool logico_3 = 2;
+	cout<<logico<<endl;
+	cout<<logico_2<<endl;
+	cout<<logico_3<<endl;
+	
+	
+	//Array
+	char cadena[6] = "Texto";
+	string cadena_texto = "Hola soy Alex";
+	cout<<"cadena: "<<cadena<<endl;
+	cout<<"string: "<<cadena_texto<<endl;
+	
+	char saludo[4];
+	saludo[0]= 'H';
+	saludo[1]= 'O';
+	saludo[2]= 'L';
+	saludo[3]= 'A';
+	cout<<"array: "<<saludo<<endl;
+	cout<<"array: "<<saludo[3]<<endl;
+	cout<<"array: "<<saludo[0]<<saludo[1]<<saludo[2]<<saludo[3]<<endl;
+	
+	int codigo[3];
+	codigo[0] = 1280;
+	codigo[1] = 3800;
+	codigo[2] = 12451280;
+	cout<<"codigo: "<<codigo[0]<<codigo[1]<<codigo[2]<<endl;
+	
+	string paises[3];
+	paises[0] = "Guatemala ";
+	paises[1] = "El Salvador ";
+	paises[2] = "Honduras ";
+	cout<<"paises: "<<paises[0]<<paises[1]<<paises[2]<<endl;
+	*/
+}
